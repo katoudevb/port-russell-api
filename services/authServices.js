e44f14e5e20f19de30543527d56c7d1723b0ae37
Katoudevb <katoudevb@gmail.com>
2025-07-15 10:42:36 +0200

    fix: correction casse sur require ../models/user

--- services/authServices.js
+++ services/authServices.js
@@ -1,4 +1,4 @@
-const User = require('../models/User');
+const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 
 const JWT_SECRET = process.env.JWT_SECRET || 'votre_clé_secrète';
